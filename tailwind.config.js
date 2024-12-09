53994dbcf2d7e61854b35c58ede54811136ebd74
lucyos <felipemartinsoliveira@gmail.com>
2024-12-09 07:55:32 -0300

    instalando mary ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		 './storage/framework/views/*.php',
+		 './resources/**/*.blade.php',
+		 './resources/**/*.js',
+		 './resources/**/*.vue',
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
     theme: {
         extend: {
             fontFamily: {
@@ -20,6 +21,8 @@ export default {
         },
     },
     plugins: [
-        forms, typography
-    ],
+		forms,
+		typography,
+		require("daisyui")
+	],
 };
