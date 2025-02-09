33a80ecd41e3f2812f914db69593c9c2234a6967
lucyos <felipemartinsoliveira@gmail.com>
2025-02-09 08:41:39 -0300

    alterando pint

--- storage/rector/66/a4/66a427d1c3823ff572e70ae4813e28653b878bee.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types = 1);
-
-return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
-   'variableKey' => 'file_hash',
-   'data' => '02dd5ba349192561151e88ced4185161',
-));
